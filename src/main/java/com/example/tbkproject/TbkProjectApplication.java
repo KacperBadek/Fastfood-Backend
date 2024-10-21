31f12e070559236612ad847ce283bad06354e244
KacperBadek <k.badek2001@gmail.com>
2024-10-21 20:52:32 +0200

    Initial commit

---
+++ src/main/java/com/example/tbkproject/TbkProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.tbkproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbkProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbkProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tbkproject/TbkProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tbkproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbkProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
